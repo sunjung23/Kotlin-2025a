8f5d26e98d507806e8a26909bf26b361c53b9bab
sunjung <sunjeong2201@gmail.com>
2025-09-19 12:36:33 +0900

    week03 : README.md & LICENSE.md v0.3

--- app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kotlinbasics
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kotlinbasics.ui.theme.KotlinBasicsTheme
+import kotlin.math.log
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,8 +31,8 @@ class MainActivity : ComponentActivity() {
         }
         // week02Variables()
         // week02Functions()
-        // week03Classes()
-        week03Collections()
+        week03Classes()
+        // week03Collections()
     }
 }
 
@@ -64,27 +66,51 @@ private fun week03Collections() {
 private fun week03Classes() {
     println("== Kotlin Classes ==")
 
-    class Student() {
-        var name: String = ""
-        var age: Int = 0
+    class Person(val name: String, var age : Int) {
+        fun introduce(){
+            Log.d("kotlinWeek03", "안녕하세요, $name, ($age 세) 입니다.")
+        }
+        fun birthday() {
+            age++
+            Log.d("KotlinWeek03", "$name 의 생일! 이제 $age 세...")
+        }
+    }
+    val person1 = Person("홍길동", 27)
+    person1.introduce()
+    person1.birthday()
 
-        fun introduce() {
-            println("Hi, I'm $name and $age years old")
+    open class Animal(var species: String) {
+        var weight: Double = 0.0
+        constructor(species: String, weight: Double) : this(species){
+            this.weight = weight
+            Log.d("KotlinWeek03", "$species 의 무게: $weight kg")
+        }
+        open fun makeSound(){
+            Log.d("KotlinWeek03", "$species 가 소리를 냅니다.")
 
         }
     }
-    val student = Student()
-    student.name = "Mirae"
-    student.age = 21
-    student.introduce()
+    val puppy = Animal("웰시코기", 10.5)
+    puppy.makeSound()
 
-    data class Person(val name: String, val age: Int)
+    class Dog(species: String, weight: Double, val breed: String) : Animal(species, weight){
+        override fun makeSound(){
+            Log.d("KotlinWeek03", "$breed($species) 가 멍멍 짖습니다")
+        }
+    }
 
-    val person1 = Person( "Kim", 23)
-    val person2 = Person( "Kim", 23)
+    val dog = Dog("개", 12.5, "골든 리트리버")
+    dog.makeSound()
 
-    println("Person1: $person1")
-    println("Equal? ${person1 == person2}")
+    data class Book(val title: String, val author: String, val pages: Int)
+
+    val book1 = Book("코틀린 입문", "Kin", 400)
+    val book2 = Book("코틀린 입문", "Kin", 400)
+
+    Log.d("KotlinWeek03", "book1 == book2: ${book1 == book2}")
+    Log.d("KotlinWeek03", "book1:$book1");
+
+
 }
 private fun week02Functions(){
 //    println("Week 02: Functions")
